e50bc3213158ce868b248fe086a85cc950809069
Navudu Narayana Rao <narayanarao15823@gmail.com>
2023-07-17 20:29:24 +0530

    some api link + index change in  both

--- client/src/helper/apiCall.js
+++ client/src/helper/apiCall.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = process.env.REACT_APP_SERVER_DOMAIN;
+axios.defaults.baseURL = "http://localhost:5000/api";
 
 const fetchData = async (url) => {
   const { data } = await axios.get(url, {
